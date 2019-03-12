cf76666b156e525bae30dd09b3fe7783536390c5
Konrad Merz <konrad@graylog.com>
2019-03-12 10:54:42 +0100

    Fix failing tests!

--- graylog2-web-interface/src/components/content-packs/ContentPackInstall.jsx
+++ graylog2-web-interface/src/components/content-packs/ContentPackInstall.jsx
@@ -140,13 +140,13 @@ class ContentPackInstall extends React.Component {
             {parameterInput}
           </Col>
         </Row>}
-        <Row>
-          <Col smOffset={1} sm={10}>
-            <ContentPackEntitiesList contentPack={contentPack} readOnly />
-          </Col>
-        </Row>
-        <button style={{ display: 'none' }} type="submit" />
       </form>
+      <Row>
+        <Col smOffset={1} sm={10}>
+          <ContentPackEntitiesList contentPack={contentPack} readOnly />
+        </Col>
+      </Row>
+      <button style={{ display: 'none' }} type="submit" />
     </div>);
   }
 }
